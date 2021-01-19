1bd8a2f11137aa7534523ec3f0410641bad65535
supriya <supriya6iem@gmail.com>
2021-01-19 21:27:15 +0530

    first commit

--- server.js
+++ server.js
@@ -31,7 +31,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 app.use('/api', routes);
 
-
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static('client/build'));
+    const path=require('path');
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
